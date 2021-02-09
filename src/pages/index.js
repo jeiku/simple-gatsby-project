02bb824675663a4ce4addb994b8d72ce46c2ed9c
jake <9012257+jeiku@users.noreply.github.com>
2021-02-08 20:08:38 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => {
+  return (
+    <Layout page="Home">
+      <h1>This is a practice Gatsby project!</h1>
+      <p>
+        Hello, I'm Jake. A full-stack developer. Guess you stumbled upon this
+        page!
+      </p>
+    </Layout>
+  )
+}
+
+export default IndexPage
